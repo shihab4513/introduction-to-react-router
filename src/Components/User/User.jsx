49e5c407621c080b45d334bda874331737af3933
shihab4513 <shihab4513.uiu@gmail.com>
2024-12-09 16:53:54 +0600

    In this commit we have learned about dynamic param with single user data

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './User.css'
+import { Link } from 'react-router-dom';
 
 const User = ({user}) => {
     const {id,name,phone,email}=user;
@@ -8,6 +9,10 @@ const User = ({user}) => {
         <h2>Name: {name}</h2>
         <h2>Email: {email}</h2>
         <p>Phone: {phone}</p>
+        <Link to={`/user/${id}`}>Show Details</Link>
+        <Link to={`/user/${id}`}>
+        <button>Click Me</button>
+        </Link>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Home from './Components/Home/Home.jsx';
 import About from './Components/About/About.jsx';
 import Contact from './Components/Contact/Contact.jsx';
 import Users from './Components/Users/Users.jsx';
+import UserDetails from './Components/UserDetails/UserDetails.jsx';
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,14 @@ const router = createBrowserRouter([
         // here we are loading data from api using loader in user component
         loader:()=>fetch('https://jsonplaceholder.typicode.com/users'),
         element:<Users></Users>
+      },
+      {
+        // here ':userId' means userId used as variable. colon before userId signify it as a variable
+        path:'/user/:userId',
+        // here in loader userId passed as params in object. to get exact userId we have to write params.userId
+        // loader:({params})=>console.log(params.userId),
+        loader:({params})=>fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element:<UserDetails></UserDetails>
       }
     ]
   }

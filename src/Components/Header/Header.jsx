44c2395ef60c21dcde1b1302a76a8ddb96ad8b52
shihab4513 <shihab4513.uiu@gmail.com>
2024-12-13 12:01:31 +0600

    In this commit we have learned Active route, Loading Spinner using navigaiton, useLocation, useNavigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../Header/Header.css'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -12,11 +12,17 @@ const Header = () => {
             <a href="/about">About</a>
             <a href="/contact">Contact Us</a> */}
             {/* There is a problem with anchor , if we want to go another page , its always reload. To overcome this problem we will use <Link> tag. Example of it shown below */}
-            <Link to={'/'}>Home</Link>
+            {/* <Link to={'/'}>Home</Link>
             <Link to={'/users'}>Users</Link>
             <Link to={'/posts'}>Posts</Link>
             <Link to={'/about'}>About</Link>
-            <Link to={'contact'}>Contact Us</Link>
+            <Link to={'contact'}>Contact Us</Link> */}
+            {/* NavLink basically add a class name 'active' if i clicked it in , based on this class we can design the text in css file */}
+            <NavLink to={'/'}>Home</NavLink>
+            <NavLink to={'/users'}>Users</NavLink>
+            <NavLink to={'/posts'}>Posts</NavLink>
+            <NavLink to={'/about'}>About</NavLink>
+            <NavLink to={'contact'}>Contact Us</NavLink>
         </nav>
     </div>
   )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigation } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 
 const Home = () => {
+  const navigation=useNavigation();
+  // Below useLocation hook will show which path/location i am currently in
+  const location=useLocation();
+  console.log(location)
   return (
     <div>
         <Header></Header>
         {/* <h2>This is home component</h2> */}
         {/* Here outlet means until outlet everything will remain same even if we go to another path like /contact etc.This means that above content will remain same. Only change will happen
         based on child path in outlet */}
-        <Outlet></Outlet>
+        {/* below code will show Loading... text when navigation state is loading else it will show outlet */}
+        {
+          navigation.state==="loading"?
+          <p>Loading...</p>:<Outlet></Outlet>
+        }
+        
         <Footer></Footer>
     </div>
     
